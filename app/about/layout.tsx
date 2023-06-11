f1f5bada891be34177ac171d780c936a52e511c0
StrongBeauty <lonely__wind@mail.ru>
2023-06-11 21:30:16 +0300

    next.js basis

---
+++ app/about/layout.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+type Props = {
+    children: React.ReactNode
+}
+
+export default function AboutLayout({ children }: Props) {
+    return (
+        <div>
+            <h1>About us</h1>
+            <ul>
+                <li>
+                    <Link href="/about/contacts">
+                        Contacts
+                    </Link>
+                </li>
+
+                <li>
+                    <Link href="/about/team">
+                        Team
+                    </Link>
+                </li>
+            </ul>
+            {children}
+        </div>
+    )
+}
